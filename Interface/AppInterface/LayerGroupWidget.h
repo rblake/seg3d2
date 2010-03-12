669b455c960f7feef67cdab2971bb728dbe002b6
wchristiansen <wchristiansen@305cdda6-5ce1-45b3-a98d-dfc68c8b3305>
2010-03-12 18:13:14 +0000

    redesign of the layermanagerwidget structure, and design for the layergroup, and layer's
    git-svn-id: https://gforge.sci.utah.edu/svn/seg3d2/trunk/src@215 305cdda6-5ce1-45b3-a98d-dfc68c8b3305

--- Interface/AppInterface/LayerGroupWidget.h
+++ Interface/AppInterface/LayerGroupWidget.h
@@ -1,22 +1,22 @@
 /*
  For more information, please see: http://software.sci.utah.edu
-
+ 
  The MIT License
-
+ 
  Copyright (c) 2009 Scientific Computing and Imaging Institute,
  University of Utah.
-
-
+ 
+ 
  Permission is hereby granted, free of charge, to any person obtaining a
  copy of this software and associated documentation files (the "Software"),
  to deal in the Software without restriction, including without limitation
  the rights to use, copy, modify, merge, publish, distribute, sublicense,
  and/or sell copies of the Software, and to permit persons to whom the
  Software is furnished to do so, subject to the following conditions:
-
+ 
  The above copyright notice and this permission notice shall be included
  in all copies or substantial portions of the Software.
-
+ 
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
@@ -26,50 +26,55 @@
  DEALINGS IN THE SOFTWARE.
  */
 
-#ifndef APPLICATION_LAYER_DATALAYER_H
-#define APPLICATION_LAYER_DATALAYER_H
+#ifndef INTERFACE_APPINTERFACE_LAYERGROUPWIDGET_H
+#define INTERFACE_APPINTERFACE_LAYERGROUPWIDGET_H
 
-#if defined(_MSC_VER) && (_MSC_VER >= 1020)
-#pragma once
-#endif
+// QT Includes
+#include <QtGui>
 
 // Application includes
-#include <Application/Layer/Layer.h>
-#include <Utils/Volume/Volume.h>
+#include <Application/Layer/LayerGroup.h>
+//#include <Application/Layer/LayerManager.h>
+#include <Interface/AppInterface/LayerWidget.h>
 
 namespace Seg3D
 {
 
-// CLASS DataLayer
+class LayerGroupWidgetPrivate;
 
-// Forward declarations
-class DataLayer;
-typedef boost::shared_ptr< DataLayer > DataLayerHandle;
-
-// Class definition
-class DataLayer : public Layer
+class LayerGroupWidget : public QWidget
 {
-
+	Q_OBJECT
+	
 	// -- constructor/destructor --
 public:
-
-	DataLayer( const std::string& name, const Utils::VolumeHandle& volume );
-	virtual ~DataLayer();
-
-	// -- state variables --
+	LayerGroupWidget( QWidget* parent, LayerGroupHandle group );
+	virtual ~LayerGroupWidget();
+	
 public:
-
-	// State describing contrast
-	StateRangedDoubleHandle contrast_state_;
-
-	// State describing brightness
-	StateRangedDoubleHandle brightness_state_;
-
-	// State describing whether volume is volume rendered
-	StateBoolHandle volume_rendered_state_;
-
+	QVector< LayerWidget_handle > layer_list_;
+	void add_layer( LayerHandle layer );
+	std::string &get_group_id();
+	
+public Q_SLOTS:
+	void show_layers( bool show );
+	void show_resample( bool show );
+	void show_transform( bool show );
+	void show_crop( bool show );
+	void show_flip_rotate( bool show );
+	void show_delete( bool show );
+	void show_selection_checkboxes( bool show );
+	void enable_delete_button( bool enable );
+	
+	
+	// -- widget internals --
+private:
+	LayerGroupWidgetPrivate* private_;	
+		
 };
+		
 
-} // end namespace Seg3D
+
+} //end namespace Seg3D
 
 #endif
