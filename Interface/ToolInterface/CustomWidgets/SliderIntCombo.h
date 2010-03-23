5fefaefe556594b8a4367d6579554a83d70f7f3d
wchristiansen <wchristiansen@305cdda6-5ce1-45b3-a98d-dfc68c8b3305>
2010-03-23 00:58:18 +0000

    Lots of ui tweaks for all the tools, layergroupwidget, and layerwidget as well as updates for connecting tools and widgets to the state engine.
    git-svn-id: https://gforge.sci.utah.edu/svn/seg3d2/trunk/src@241 305cdda6-5ce1-45b3-a98d-dfc68c8b3305

--- Interface/ToolInterface/CustomWidgets/SliderIntCombo.h
+++ Interface/ToolInterface/CustomWidgets/SliderIntCombo.h
@@ -1,22 +1,22 @@
 /*
  For more information, please see: http://software.sci.utah.edu
- 
+
  The MIT License
- 
+
  Copyright (c) 2009 Scientific Computing and Imaging Institute,
  University of Utah.
- 
- 
+
+
  Permission is hereby granted, free of charge, to any person obtaining a
  copy of this software and associated documentation files (the "Software"),
  to deal in the Software without restriction, including without limitation
  the rights to use, copy, modify, merge, publish, distribute, sublicense,
  and/or sell copies of the Software, and to permit persons to whom the
  Software is furnished to do so, subject to the following conditions:
- 
+
  The above copyright notice and this permission notice shall be included
  in all copies or substantial portions of the Software.
- 
+
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
@@ -26,37 +26,54 @@
  DEALINGS IN THE SOFTWARE.
  */
 
-#ifndef INTERFACE_TOOLINTERFACE_GRADIENTMAGNITUDEFILTERINTERFACE_H
-#define INTERFACE_TOOLINTERFACE_GRADIENTMAGNITUDEFILTERINTERFACE_H
+#ifndef INTERFACE_TOOLINTERFACE_CUSTOMWIDGETS_SLIDERINTCOMBO_H
+#define INTERFACE_TOOLINTERFACE_CUSTOMWIDGETS_SLIDERINTCOMBO_H
 
-// Utils includes
-#include <Utils/Core/Log.h>
+// QT Includes
+#include <QtGui>
 
-// Application includes
-#include <Application/Tool/ToolFactory.h>
+#include <boost/shared_ptr.hpp>
 
-// Base class of the tool widget
-#include <Interface/AppInterface/ToolWidget.h>
-
-namespace Seg3D
+namespace Seg3D 
 {
 
-class GradientMagnitudeFilterInterfacePrivate;
+class SliderIntComboPrivate;
 
-class GradientMagnitudeFilterInterface : public ToolWidget
+class SliderIntCombo : public QWidget
 {
 Q_OBJECT
 
+Q_SIGNALS:
+	void valueAdjusted( int );
+	void rangeChanged( int );
+
+// -- constructor/destructor --
 public:
-	GradientMagnitudeFilterInterface();
-	virtual ~GradientMagnitudeFilterInterface();
-	virtual bool build_widget( QFrame* frame );
+    SliderIntCombo( QWidget* parent = 0, bool edit_range = false );
+    virtual ~SliderIntCombo();
+    
+public Q_SLOTS:
+    void setStep(int);
+	void setRange( int, int );
+	void setCurrentValue( int );
+	void set_all( int min, int max, int value );
 
+    
+// -- widget internals -- 
 private:
-    boost::shared_ptr< GradientMagnitudeFilterInterfacePrivate > private_;
-
+    boost::shared_ptr< SliderIntComboPrivate > private_;
+    
+private Q_SLOTS:
+    void edit_ranges( bool edit );
+    void change_min( int new_min );
+    void change_max( int new_max );
+    void double_range();
+    void half_range();
+    void slider_signal( int value );
+    void spinner_signal( int value );
+  
 };
 
-} // end namespace Seg3D
+}  // end namespace Seg3D
 
 #endif
