e4fe23d655b4ff822206e9083bcc0802ac7eef18
jeroen <jeroen@305cdda6-5ce1-45b3-a98d-dfc68c8b3305>
2010-11-03 05:22:29 +0000

    first series of commits for doing undo/redo
    git-svn-id: https://gforge.sci.utah.edu/svn/seg3d2/trunk@800 305cdda6-5ce1-45b3-a98d-dfc68c8b3305

--- src/Application/LayerManager/LayerCheckPoint.cc
+++ src/Application/LayerManager/LayerCheckPoint.cc
@@ -1,22 +1,22 @@
 /*
  For more information, please see: http://software.sci.utah.edu
- 
+
  The MIT License
- 
+
  Copyright (c) 2009 Scientific Computing and Imaging Institute,
  University of Utah.
- 
- 
+
+
  Permission is hereby granted, free of charge, to any person obtaining a
  copy of this software and associated documentation files (the "Software"),
  to deal in the Software without restriction, including without limitation
  the rights to use, copy, modify, merge, publish, distribute, sublicense,
  and/or sell copies of the Software, and to permit persons to whom the
  Software is furnished to do so, subject to the following conditions:
- 
+
  The above copyright notice and this permission notice shall be included
  in all copies or substantial portions of the Software.
- 
+
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
@@ -26,54 +26,64 @@
  DEALINGS IN THE SOFTWARE.
  */
 
-#ifndef APPLICATION_FILTERS_ACTIONS_ACTIONINVERT_H
-#define APPLICATION_FILTERS_ACTIONS_ACTIONINVERT_H
-
-#include <Core/Action/Actions.h>
+#include <Application/LayerManager/LayerCheckPoint.h>
+#include <Application/LayerManager/LayerManager.h>
 
 namespace Seg3D
 {
-	
-class ActionInvert : public Core::Action
-{
 
-CORE_ACTION( 
-	CORE_ACTION_TYPE( "Invert", "Invert the values of the data layer" )
-	CORE_ACTION_ARGUMENT( "layerid", "The layerid on which this tool needs to be run." )
-	CORE_ACTION_KEY( "replace", "true", "Replace the old layer (true), or add an new layer (false)" )
-	CORE_ACTION_CHANGES_PROJECT_DATA()
-	CORE_ACTION_IS_UNDOABLE()
-)
-	
-	// -- Constructor/Destructor --
+class LayerCheckPointPrivate : public boost::noncopyable
+{
 public:
-	ActionInvert()
+		Core::VolumeHandle volume_;
+};
+
+
+LayerCheckPoint::LayerCheckPoint( LayerHandle layer ) :
+	private_( new LayerCheckPointPrivate )
+{
+	this->create_volume( layer );
+}
+
+LayerCheckPoint::LayerCheckPoint( LayerHandle layer, Core::VolumeSliceType slice_type, size_t idx ) :
+	private_( new LayerCheckPointPrivate )
+{
+	this->create_slice( layer, slice_type, idx );
+}
+
+LayerCheckPoint::~LayerCheckPoint()
+{
+}
+	
+bool LayerCheckPoint::apply( LayerHandle layer ) const
+{
+	// If there is a full volume in the check point insert it into the layer
+	if ( this->private_->volume_ )
 	{
-		// Action arguments
-		this->add_argument( this->layer_id_ );
-		
-		// Action options
-		this->add_key( this->replace_ );
+		if ( layer->get_type() == Core::VolumeType::MASK_E )
+		{
+			LayerManager::DispatchInsertVolumeIntoLayer( layer, this->private_->volume_ );
+		}
 	}
 	
-	virtual ~ActionInvert() {}
+	return true;
+}
 	
-	// -- Functions that describe action --
-public:
-	virtual bool validate( Core::ActionContextHandle& context );
-	virtual bool run( Core::ActionContextHandle& context, Core::ActionResultHandle& result );
-	
-	// -- Action parameters --
-private:
+bool LayerCheckPoint::create_volume( LayerHandle layer )
+{
+	this->private_->volume_ = layer->get_volume();
+	return false;
+}
 
-	Core::ActionParameter< std::string > layer_id_;
-	Core::ActionParameter< bool > replace_;
-	
-public:
-	static void Dispatch( Core::ActionContextHandle context, 
-		std::string layer_id, bool replace );
-};
+bool LayerCheckPoint::create_slice( LayerHandle layer, Core::VolumeSliceType slice_type, size_t idx )
+{
+	return false;
+}
+
+size_t LayerCheckPoint::get_byte_size() const
+{
+	return 0;
+}
+
 	
 } // end namespace Seg3D
-
-#endif
