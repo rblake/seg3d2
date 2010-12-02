96f0edfa817d23508a102a8796e3143063cf5f87
jshu <jshu@305cdda6-5ce1-45b3-a98d-dfc68c8b3305>
2010-12-02 12:43:30 +0000

    Added Python 3.1.2 source code to the external library. Embedded Python interpreter into Seg3D and implemented simple mechanism for running actions in python. NOTE: This has only been tested on Windows.
    git-svn-id: https://gforge.sci.utah.edu/svn/seg3d2/trunk/src@887 305cdda6-5ce1-45b3-a98d-dfc68c8b3305

---
+++ Externals/python/PC/pythonnt_rc.h
@@ -0,0 +1,8 @@
+/* This file created by make_versioninfo.exe */
+#define FIELD3 2150
+#define MS_DLL_ID "3.1"
+#ifndef _DEBUG
+#define PYTHON_DLL_NAME "python31.dll"
+#else
+#define PYTHON_DLL_NAME "python31_d.dll"
+#endif
