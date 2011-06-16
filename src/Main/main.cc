d5f97790e87dd280d042624e15f90e5bbbad15a5
jeroen <jeroen@305cdda6-5ce1-45b3-a98d-dfc68c8b3305>
2011-06-16 00:50:16 +0000

    update release numbers and remove message from main
    git-svn-id: https://gforge.sci.utah.edu/svn/seg3d2/branches/2.0.4@1485 305cdda6-5ce1-45b3-a98d-dfc68c8b3305

--- src/Main/main.cc
+++ src/Main/main.cc
@@ -145,17 +145,17 @@ int main( int argc, char **argv )
 	std::string file_to_view = "";
 	Core::Application::Instance()->check_command_line_parameter( "file_to_open_on_start", file_to_view );
 
-	{
-		std::string warning = std::string( "<h3>" ) +
-			Core::Application::GetApplicationName() + " " + Core::Application::GetVersion() + 
-			"</h3><h6><p align=\"justify\">Please note: This version of " + Core::Application::GetApplicationName()
-			+ " is still under development. For daily use we recommend the released version, as" 
-			" stability of this version depends on on going development.</p></h6>";
-		
-		QMessageBox::information( 0, 
-			QString::fromStdString( Core::Application::GetApplicationNameAndVersion() ), 
-			QString::fromStdString( warning )  );
-	}
+//	{
+//		std::string warning = std::string( "<h3>" ) +
+//			Core::Application::GetApplicationName() + " " + Core::Application::GetVersion() + 
+//			"</h3><h6><p align=\"justify\">Please note: This version of " + Core::Application::GetApplicationName()
+//			+ " is still under development. For daily use we recommend the released version, as" 
+//			" stability of this version depends on on going development.</p></h6>";
+//		
+//		QMessageBox::information( 0, 
+//			QString::fromStdString( Core::Application::GetApplicationNameAndVersion() ), 
+//			QString::fromStdString( warning )  );
+//	}
 	
 
 	if ( sizeof( void * ) == 4 )
