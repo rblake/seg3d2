d4dd4bebb2280f825c021d63c7a88a249c44ba4c
jshu <jshu@305cdda6-5ce1-45b3-a98d-dfc68c8b3305>
2011-07-20 22:24:14 +0000

    Optimized the performance of provenance replay.
    git-svn-id: https://gforge.sci.utah.edu/svn/seg3d2/trunk@1563 305cdda6-5ce1-45b3-a98d-dfc68c8b3305

--- src/Core/Utils/StringContainer.h
+++ src/Core/Utils/StringContainer.h
@@ -1,22 +1,22 @@
 /*
  For more information, please see: http://software.sci.utah.edu
- 
+
  The MIT License
- 
+
  Copyright (c) 2009 Scientific Computing and Imaging Institute,
  University of Utah.
- 
- 
+
+
  Permission is hereby granted, free of charge, to any person obtaining a
  copy of this software and associated documentation files (the "Software"),
  to deal in the Software without restriction, including without limitation
  the rights to use, copy, modify, merge, publish, distribute, sublicense,
  and/or sell copies of the Software, and to permit persons to whom the
  Software is furnished to do so, subject to the following conditions:
- 
+
  The above copyright notice and this permission notice shall be included
  in all copies or substantial portions of the Software.
- 
+
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
@@ -26,52 +26,32 @@
  DEALINGS IN THE SOFTWARE.
  */
 
-#include <Core/Interface/Interface.h>
+#ifndef CORE_UTILS_STRINGCONTAINER_H
+#define CORE_UTILS_STRINGCONTAINER_H
 
-#include <Interface/Application/ControllerContext.h>
+#if defined(_MSC_VER) && (_MSC_VER >= 1020)
+# pragma once
+#endif 
 
-namespace Seg3D
+// STL includes
+#include <list>
+#include <string>
+#include <vector>
+
+// boost includes
+#include <boost/shared_ptr.hpp>
+
+namespace Core
 {
 
-ControllerContext::ControllerContext( ControllerInterface* controller ) :
-	controller_( controller )
-{
-}
+typedef std::vector< std::string > StringVector;
+typedef boost::shared_ptr< StringVector > StringVectorHandle;
+typedef boost::shared_ptr< const StringVector > StringVectorConstHandle;
 
-ControllerContext::~ControllerContext()
-{
-}
+typedef std::list< std::string > StringList;
+typedef boost::shared_ptr< StringList > StringListHandle;
+typedef boost::shared_ptr< const StringList > StringListConstHandle;
 
-void ControllerContext::report_error( const std::string& error )
-{
-	ControllerInterface::PostActionMessage( controller_, error );
-}
+} // End namespace Core
 
-void ControllerContext::report_warning( const std::string& warning )
-{
-	ControllerInterface::PostActionMessage( controller_, warning );
-}
-
-void ControllerContext::report_message( const std::string& message )
-{
-	ControllerInterface::PostActionMessage( controller_, message );
-}
-
-void ControllerContext::report_need_resource( Core::NotifierHandle notifier )
-{
-	std::string message = std::string( "'" ) + notifier->get_name() + std::string(
-	    "' is currently unavailable" );
-	ControllerInterface::PostActionMessage( controller_, message );
-}
-
-void ControllerContext::report_done()
-{
-	if ( is_success() ) ControllerInterface::PostActionMessage( controller_, "" );
-}
-
-Core::ActionSource ControllerContext::source() const
-{
-	return Core::ActionSource::COMMANDLINE_E;
-}
-
-} //end namespace Seg3D
+#endif
