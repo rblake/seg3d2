3133b740982bf249c6d18a7451740dedccf0f5dd
ayla <ayla@305cdda6-5ce1-45b3-a98d-dfc68c8b3305>
2015-01-26 16:13:04 +0000

    Original copy of Shankar's API code.
    git-svn-id: https://gforge.sci.utah.edu/svn/seg3d2/branches/rbf_project@2151 305cdda6-5ce1-45b3-a98d-dfc68c8b3305

---
+++ src/Plugins/rbf/Application/Tools/src/API.h
@@ -0,0 +1,31 @@
+#ifndef _API_H_
+#define _API_H_
+
+#include <vector>
+#include <string>
+
+#include "ScatteredData.h"
+#include "vec3.h"
+#include "SampleData.h"
+#include "RBF.h"
+#include "horizon.h"
+
+using std::string; 
+
+class API
+{
+	public:
+	ScatteredData *mySurfaceData;
+	Horizon *mySurface;
+	RBF *mySurfaceRBF;
+	Kernel myKernel;
+	vector<vector<vector<double> > > value;
+
+	void CreateSurface(string filename, vec3 myOrigin, vec3 mySize, vec3 mySampling);
+	void CreateSurface(vector<vec3> myData, vec3 myOrigin, vec3 mySize, vec3 mySampling);
+	API(string filename, string dimensions);
+	API(vector<vec3> myData, vec3 myOrigin, vec3 mySize, vec3 mySampling);
+	API();
+};
+
+#endif //_API_H_ 
